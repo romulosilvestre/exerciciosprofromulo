56004671bdfd6f7f32cbc55a5bf3f3fedcdf871c
romulo.leite <romulo.silvestre@df.estudante.senai.br>
2022-07-06 11:58:42 -0300

    exercícios

---
+++ ExerciciosProfRomulo.EscolhinhaTicTic/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.EscolhinhaTicTic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declaração de variáveis
+            string nomeDoAluno,resultado;
+            double nota1, nota2, nota3;
+            double media;
+
+            //Entrada de Dados
+            Console.WriteLine("Abençoado qual o seu nome:");
+            nomeDoAluno = Console.ReadLine();
+            Console.WriteLine("Digite três notas");
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+            nota3 = double.Parse(Console.ReadLine());
+            media = (nota1 + nota2 + nota3) / 3;
+            /*if (media >= 7)
+            {
+                resultado = "aprovado";
+            }
+            else
+            {
+                resultado = "reprovado";
+            }*/
+            resultado = media >= 7 ? "aprovado" : "reprovado";
+
+            Console.WriteLine($"O aluno {nomeDoAluno} obteve a média {media} e foi {resultado}");
+
+            /*
+            string[] alunos = { "João", "Maria", "José","Rômulo","Ramon","Bruno","William","Yedo","Jeferson" };
+            //Console.WriteLine(alunos[1]);
+            int cont=0;
+            foreach (var item in alunos)
+            {
+                int r = item.StartsWith("R") ? cont++ : 0;
+
+            }
+            Console.WriteLine($"Foram {cont} alunos que tem o nome iniciando com a letra R ");
+
+            foreach (var item in alunos)
+            {
+                if (item.StartsWith("R"))
+                {
+                    Console.WriteLine(item);
+                }
+                
+            }*/
+            Console.ReadKey();
+
+            
+        }
+      
+    }
+}
---
+++ ExerciciosProfRomulo.MatematicaBasica/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.MatematicaBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exercício 3- No projeto MatematicaBasica crie um 
+               Programa que calcule a soma de dois números inteiros.
+               Considere que o usuário vai digitar esses dois números.
+            */
+            //declara as variáveis
+            int num1;
+            int num2;
+            int soma;
+            //entrada de dados
+            Console.WriteLine("Jovem Jovem digite dois números");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            soma = num1 + num2;
+            Console.WriteLine(soma);
+        }
+    }
+}
